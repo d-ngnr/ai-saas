fe062626bf0aee299bfdaea3bee3191b926ca81c
d-ngnr <realngnr@gmail.com>
2024-05-30 19:20:26 -0500

    Add shadcn button

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function LandingPage() {
-  return <div>Landing Page (Unprotected)</div>;
+  return (
+    <div>
+      Landing Page (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
