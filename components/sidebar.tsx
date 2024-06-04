8f2a8f6bd47b9e5de9133f89d6acefdb173923c9
d-ngnr <realngnr@gmail.com>
2024-06-03 21:33:10 -0500

    Add Sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex-col h-full bg-[#111827] text-white">
+      hello sidebar component
+    </div>
+  );
+};
