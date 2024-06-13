c5901fabbdc0fa8ae78d5de02c8e15647241e2d5
d-ngnr <realngnr@gmail.com>
2024-06-12 21:22:37 -0500

    Fix image of the SaaS app

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -66,9 +66,9 @@ const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex-items-center pl-3 mb-14">
+        <Link href="/dashboard" className="flex flex-items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
-            {/* <Image fill alt="Logo" src="/next.svg" /> */}
+            <Image fill alt="Logo" src="/vercel.svg" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             Genius
